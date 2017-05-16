c4df0ca66540c10e61a61220aaf93dcdbcc4e86c
Jack <Jackramills@gmail.com>
2017-05-16 23:32:41 +0100

    Add some design and accent colour

--- js/main.js
+++ js/main.js
@@ -109,6 +109,117 @@ particlesJS('particles-js', {
   'retina_detect': true
 });
 
+particlesJS('particles-js2', {
+  'particles': {
+    'number': {
+      'value': 10,
+      'density': {
+        'enable': true,
+        'value_area': 700
+      }
+    },
+    'color': {
+      'value': '#777777'
+    },
+    'shape': {
+      'type': 'circle',
+      'stroke': {
+        'width': 2,
+        'color': '#3abecb'
+      },
+      'polygon': {
+        'nb_sides': 5
+      },
+      'image': {
+        'src': 'img/github.svg',
+        'width': 100,
+        'height': 100
+      }
+    },
+    'opacity': {
+      'value': 0,
+      'random': false,
+      'anim': {
+        'enable': false,
+        'speed': 1,
+        'opacity_min': 0.1,
+        'sync': false
+      }
+    },
+    'size': {
+      'value': 15,
+      'random': true,
+      'anim': {
+        'enable': false,
+        'speed': 40,
+        'size_min': 0.1,
+        'sync': false
+      }
+    },
+    'line_linked': {
+      'enable': false,
+      'distance': 150,
+      'color': '#ffffff',
+      'opacity': 0.4,
+      'width': 1
+    },
+    'move': {
+      'enable': true,
+      'speed': 6,
+      'direction': 'top',
+      'random': true,
+      'straight': false,
+      'out_mode': 'out',
+      'bounce': false,
+      'attract': {
+        'enable': false,
+        'rotateX': 600,
+        'rotateY': 1200
+      }
+    }
+  },
+  'interactivity': {
+    'detect_on': 'window',
+    'events': {
+      'onhover': {
+        'enable': true,
+        'mode': 'repulse'
+      },
+      'onclick': {
+        'enable': true,
+        'mode': 'push'
+      },
+      'resize': true
+    },
+    'modes': {
+      'grab': {
+        'distance': 140,
+        'line_linked': {
+          'opacity': 1
+        }
+      },
+      'bubble': {
+        'distance': 400,
+        'size': 40,
+        'duration': 2,
+        'opacity': 8,
+        'speed': 3
+      },
+      'repulse': {
+        'distance': 150,
+        'duration': 0.4
+      },
+      'push': {
+        'particles_nb': 3
+      },
+      'remove': {
+        'particles_nb': 2
+      }
+    }
+  },
+  'retina_detect': true
+});
+
 $(document).ready(function () {
   $('#home-btn, #home-btn-fixed').click(function () {
     $('html, body').animate({
