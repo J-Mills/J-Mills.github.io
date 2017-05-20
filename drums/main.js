96482171be045b7feb1b187e86082816cf1a9aec
Jack <Jackramills@gmail.com>
2017-05-20 13:24:20 +0100

    Fix drums getting stuck when key held

--- drums/main.js
+++ drums/main.js
@@ -2,10 +2,15 @@ window.addEventListener('keydown', function (e) {
   console.log(e.keyCode);
   const key = document.querySelector(`.key[data-key='${e.keyCode}']`);
   const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+
   if (!audio) return;
+
   audio.currentTime = 0;
   audio.play();
   key.classList.add('playing');
+  setTimeout(function () {
+    key.classList.remove('playing');
+  }, 1000);
 });
 
 function removeTransition (e) {
